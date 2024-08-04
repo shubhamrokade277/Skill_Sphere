cf4dcfe834b5dcaa919396b733c606d379ceaac5
Shubham <shubhamrokade277@gmail.com>
2024-08-04 13:50:09 +0530

    Backend + Frontend

---
+++ frontend-elearning/src/components/about/About.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+function About() {
+  
+  return (
+    <> 
+    <div>About Us  <br />
+      <section id="team" class="team">
+
+<h1 class="heading"><br/> Our Team</h1>
+
+<div class="row">
+
+<div class="card">
+  <div class="image">
+    {/* <img src={require("../about/Amar.jpg")}/> */}
+  </div>
+  <div class="info">
+    <h4> </h4>
+    <h4>Full Stack-Web Developer</h4>
+   
+  </div>
+</div>
+
+<div class="card">
+  <div class="image">
+    {/* <img  src={require("../about/Shiv.jpg")}/> */}
+  </div>
+  <div class="info">
+    <h4> </h4>
+    <h4>Full Stack-Web Developer</h4>
+    
+  </div>
+</div>
+
+<div class="card">
+  <div class="image">
+    {/* <img src={require("../about/Ashlekh[1].jpg")} alt=""/> */}
+  </div>
+  <div class="info">
+    <h4> </h4>
+    <h4>Full Stack-Web Developer</h4>
+    
+  </div>
+</div>
+{/* <div class="card">
+  <div class="image">
+    <img src={require("../about/recent.jpg")} alt=""/>
+  </div>
+  <div class="info">
+    <h4> </h4>
+    <h4>Full stack-Web Developer</h4>
+    
+  </div>
+</div> */}
+{/* <div class="card">
+  <div class="image">
+    <img src={require("../about/sandeep.jpeg")} alt=""/>
+  </div>
+  <div class="info">
+    <h4></h4>
+    <h4></h4>
+    
+  </div>
+</div> */}
+</div>
+</section>
+
+    </div>
+    </>
+  )
+}
+
+export default About
